39a684e20cb7a80e7c375602ee377639e8b48717
CHOI\Choi <cholnh1@naver.com>
2019-03-18 17:00:17 +0900

    asd

--- src/main/java/com/mrporter/pomangam/product/vo/ProductBean.java
+++ src/main/java/com/mrporter/pomangam/product/vo/ProductBean.java
@@ -12,9 +12,21 @@ import lombok.NoArgsConstructor;
  */
 @Data @AllArgsConstructor @NoArgsConstructor
 public class ProductBean {
+	/*
 	Integer idx, idx_restaurant, type;
 	String name;
 	Integer price;
 	String imgpath, description;
 	Integer unit_amount, unit_time, cnt_sell;
+	*/
+	Integer idx, idx_restaurant;
+	String category, name;
+	Integer price;
+	String imgpath, description;
+	Integer unit_amount, unit_time, cnt_sell;
+	Integer isActive, type; // 1 : main  / 2 : sub
+	Integer sequence;
+	String sub_description;
+	Integer c_commission_prc;
+	Integer s_commission_prc;
 }
